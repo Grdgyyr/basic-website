cd0661bdb24319b4c639848b07228ec114abf2a3
ron <ronmagpantay1@gmail.com>
2025-02-17 13:23:35 +0800

    React Web

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ResponsiveAppBar from './components/appbar/AppBar';
+import Section1 from './components/section1/Section1';
+
+function App() {
+  return (
+    <div className="App">
+      <ResponsiveAppBar />
+      <Section1 />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/section1/Section1.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Section1.css'
+
+export default function Section1() {
+  return (
+    <div className='Section1Container'>Section1</div>
+  )
+}
