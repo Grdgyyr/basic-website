369edd65693ad7cc50c3d1cd534efc2ef69bb521
ron <ronmagpantay1@gmail.com>
2025-02-18 13:39:57 +0800

    Add: Section7 component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Section1 from './components/section1/Section1';
 import Section2 from './components/section2/Section2';
 import Section3 from './components/section3/Section3';
 import Section4 from './components/section4/Section4';
+import Section7 from './components/section7/Section7';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Section2 />
       <Section3 />
       <Section4 />
+      <Section7 />
     </div>
   );
 }
