7d63b1ac95c45a17400d992db89cfa5636d1cf5d
ron <ronmagpantay1@gmail.com>
2025-02-18 09:40:12 +0800

    Add Section3 component with responsive design and styling

--- src/components/section3/Section3.js
+++ src/components/section3/Section3.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from "react";
+import { Box, Button, Typography, Container, useMediaQuery } from "@mui/material";
+import { styled } from "@mui/system";
 
-export default function Section3() {
+
+const Section3 = () => {
+  const isMobile = useMediaQuery("(max-width:768px)");
   return (
-    <div>Section3</div>
-  )
+    <Box
+      id="section3"
+      sx={{
+        minHeight: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#0D1B2A",
+        color: "white",
+        padding: "70px 0 50px",
+        position: "relative",
+        textAlign: isMobile ? "center" : "left",
+      }}
+    >
+      <h1>Section3</h1>
+    </Box>
+  );
 }
+
+export default Section3;
